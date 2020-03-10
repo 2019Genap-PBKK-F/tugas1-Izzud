b29232247fe52cc54b30a556bdb3d54f30bc208c
Izzud <m.izzuddin4132@gmail.com>
2020-03-10 21:29:16 +0700

    adjusted to tugas2

--- src/components/views/Excel.vue
+++ src/components/views/Excel.vue
@@ -6,6 +6,11 @@
       value='Add new row'
       @click='jExcelObj.insertRow()'
     />
+    <input
+      type='button'
+      value='Delete row'
+      @click='jExcelObj.deleteRow()'
+    />
     <br />
     <br />
     <div id='spreadsheet' ref='spreadsheet'></div>
@@ -24,22 +29,24 @@ export default {
       bio: [
         {
           'id': 0,
-          'nrp': 0,
-          'name': 'aaaaaaaaaaaaaaa',
-          'angkatan': 2342,
-          'dob': '',
-          'img': '',
-          'aktif': true
+          'nrp': null,
+          'nama': null,
+          'angkatan': null,
+          'gender': null,
+          'dob': null,
+          'photo': null,
+          'active': null
         }
       ],
       template: {
         'id': 0,
-        'nrp': 0,
-        'name': 'aaaaaaaaaaaaaaa',
-        'angkatan': 2342,
-        'dob': '',
-        'img': '',
-        'aktif': true
+        'nrp': null,
+        'nama': null,
+        'angkatan': null,
+        'gender': null,
+        'dob': null,
+        'photo': null,
+        'active': null
       }
     }
   },
@@ -50,21 +57,24 @@ export default {
         columns: [
           { type: 'hidden', title: 'ID', width: '120px', name: 'id' },
           { type: 'numeric', title: 'NRP', width: '120px', name: 'nrp' },
-          { type: 'text', title: 'Nama', width: '120px', name: 'name' },
+          { type: 'text', title: 'Nama', width: '120px', name: 'nama' },
           { type: 'numeric', title: 'Angkatan', width: '120px', name: 'angkatan' },
+          { type: 'text', title: 'Jenis Kelamin', width: '120px', name: 'gender' },
           { type: 'calendar', title: 'Tanggal lahir', width: '250px', name: 'dob' },
-          { type: 'image', title: 'Photo', width: '120px', name: 'img' },
-          { type: 'checkbox', title: 'Aktif', width: '80px', name: 'aktif' }
+          { type: 'image', title: 'Photo', width: '120px', name: 'photo' },
+          { type: 'checkbox', title: 'Aktif', width: '80px', name: 'active' }
         ],
         onchange: this.changed,
-        oninsertrow: this.insertedRow
+        oninsertrow: this.insertedRow,
+        ondeleterow: this.deleteRow
       }
     }
   },
   methods: {
     load() {
-      axios.get('http://localhost:3000/mahasiswa').then(response => {
-        var valuesOnly = response.data
+      axios.get('http://localhost:8007/api/mahasiswa').then(response => {
+        console.log(response)
+        var valuesOnly = response.data.recordset
         this.bio = valuesOnly
         console.log(this.bio)
       }).catch((err) => {
@@ -75,19 +85,32 @@ export default {
       x = parseInt(x)
       y = parseInt(y)
 
-      var id = y
-      var temp = {}
-      axios.get('http://localhost:3000/mahasiswa/' + id).then((response) => {
-        temp = response.data
-        var keys = Object.keys(this.template)
+      console.log('x: ' + x + '\ny: ' + y)
+
+      var id = y + 1
+      var data = {}
+      axios.get('http://localhost:8007/api/mahasiswa/' + id).then((response) => {
+        data = Object.values(response.data.recordset[0])
+        // var keys = Object.keys(this.template)
+        // console.log('key: ' + keys)
+        // console.log(temp.constructor)
         // var key = keys[x+1]
 
-        temp[keys[x]] = value
-        console.log(temp)
+        data[x] = value
+        console.log(data)
         axios({
           method: 'put',
-          url: 'http://localhost:3000/mahasiswa/' + id,
-          data: temp
+          url: 'http://localhost:8007/api/mahasiswa/' + id,
+          data: {
+            'id': data[0],
+            'nrp': data[1],
+            'nama': data[2],
+            'angkatan': data[3],
+            'gender': data[4],
+            'dob': data[5],
+            'photo': data[6],
+            'active': data[7]
+          }
         }).then((response) => {
           console.log(response.data)
         })
@@ -96,14 +119,26 @@ export default {
     insertedRow(instance) {
       axios({
         method: 'post',
-        url: 'http://localhost:3000/mahasiswa/',
+        url: 'http://localhost:8007/api/mahasiswa/',
         data: {
-          'nrp': 0,
-          'name': '',
-          'angkatan': 0,
-          'dob': '',
-          'img': '',
-          'aktif': true
+          'nrp': null,
+          'nama': null,
+          'angkatan': null,
+          'gender': null,
+          'dob': null,
+          'photo': null,
+          'active': null
+        }
+      }).then((response) => {
+        console.log(response.data)
+      })
+    },
+    deleteRow(instance, id) {
+      console.log(id)
+      axios({
+        method: 'delete',
+        url: 'http://localhost:8007/api/mahasiswa/' + id,
+        data: {
         }
       }).then((response) => {
         console.log(response.data)
